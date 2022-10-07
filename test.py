389d5305796081422d9c36b044a360f85d26cc89
shukriamohamed <110675390+shukriamohamed@users.noreply.github.com>
2022-10-07 19:34:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #!/bin/python3
-import myapp, unittest
+import app, unittest
 class TestHello(unittest.TestCase):
     def setUp(self):
-        myapp.app.testing = True
-        self.app = myapp.app.test_client()
+        app.app.testing = True
+        self.app = app.app.test_client()
 
 
     def test_hello(self):
@@ -27,4 +27,3 @@ if __name__ == '__main__':
     runner = xmlrunner.XMLTestRunner(output='test-reports')
     unittest.main(testRunner=runner)
     unittest.main()
-
